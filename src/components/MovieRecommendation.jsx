08abc00bd74a7d467029bffb050a1259db99f63d
MeetRamatri <84379308+MeetRamatri@users.noreply.github.com>
2025-04-07 16:38:49 +0530

    Update MovieRecommendation.jsx

--- src/components/MovieRecommendation.jsx
+++ src/components/MovieRecommendation.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Container, Typography, Button, Box } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
 
