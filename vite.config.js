4a4e45dd63e0eb7aca05ae463a1467a26b882e41
MeetRamatri <meetramatri12@gmail.com>
2025-04-07 14:13:25 +0530

    for deployment installed terser for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import terser from "@rollup/plugin-terser";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
+      plugins: [terser()]
     }
   }
-})
+});
